f7c9c65d022cd60cdc671c1582f6d33798655c34
Pablo Benavides <pbenav@gmail.com>
2022-05-23 19:48:52 +0200

    More refinements
    More refinements
    
    More refinements

--- app/View/Components/Clock.php
+++ app/View/Components/Clock.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\View\Components\cth;
+namespace App\View\Components;
 
 use Illuminate\View\Component;
 
@@ -23,6 +23,6 @@ class Clock extends Component
      */
     public function render()
     {
-        return view('components.cth.clock');
+        return view('components.clock');
     }
 }
