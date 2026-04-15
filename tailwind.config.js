a54255cf6fef2b84c3167e047ffe3ef917028cb0
Pablo Benavides <pbenav@gmail.com>
2026-04-15 11:37:19 +0200

    Fix: Hard-code Numpad styles in app.css to bypass import/caching issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
     // Asegurar que las clases usadas por SweetAlert2 no sean purgadas
     safelist: [
         'swal2-popup', 'swal2-modal', 'swal2-title', 'swal2-content', 'swal2-actions',
-        'swal2-confirm', 'swal2-cancel', 'swal2-icon', 'swal2-html-container'
+        'swal2-confirm', 'swal2-cancel', 'swal2-icon', 'swal2-html-container',
+        'btn-pad', 'btn-aux', 'btn-code', 'pin-input', 'col-span-3'
     ],
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
