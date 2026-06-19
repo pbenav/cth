2eb0968a11c26a6d32aa74ca68483c4678a470fb
Pablo Benavides <pbenav@gmail.com>
2026-06-19 19:27:52 +0200

    feat(settings): add toggle visibility to s2s secret and translate actions

--- resources/views/livewire/admin/app-settings.blade.php
+++ resources/views/livewire/admin/app-settings.blade.php
@@ -95,9 +95,20 @@
             </div>
 
             <!-- MTX Secret -->
-            <div class="col-span-6 sm:col-span-4">
+            <div class="col-span-6 sm:col-span-4" x-data="{ showSecret: false }">
                 <x-label for="mtx_secret" value="{{ __('Clave de Seguridad S2S') }}" />
-                <x-input id="mtx_secret" type="password" class="mt-1 block w-full bg-gray-50 dark:bg-gray-800" wire:model="state.MTX_S2S_SECRET" />
+                <div class="relative mt-1">
+                    <x-input id="mtx_secret" x-bind:type="showSecret ? 'text' : 'password'" class="block w-full bg-gray-50 dark:bg-gray-800 pr-10" wire:model="state.MTX_S2S_SECRET" />
+                    <button type="button" @click="showSecret = !showSecret" class="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-500 hover:text-gray-700">
+                        <svg x-show="!showSecret" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                        </svg>
+                        <svg x-show="showSecret" x-cloak xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />
+                        </svg>
+                    </button>
+                </div>
                 <x-input-error for="state.MTX_S2S_SECRET" class="mt-2" />
                 <p class="text-[11px] text-gray-400 mt-1">Debe coincidir exactamente con el Secret configurado en el servidor MTX.</p>
             </div>
@@ -105,11 +116,11 @@
 
         <x-slot name="actions">
             <x-action-message class="mr-3" on="saved">
-                {{ __('Saved.') }}
+                Guardado.
             </x-action-message>
 
             <x-button wire:loading.attr="disabled" wire:target="newBackground, updateSettings">
-                {{ __('Save Settings') }}
+                Guardar Configuración
             </x-button>
         </x-slot>
     </x-form-section>
